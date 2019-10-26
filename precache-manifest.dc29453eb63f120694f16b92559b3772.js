8456c827b1e61270f1c40f8945d3f04da54b7cc9
febarrientosg <fbarrientos02@gmail.com>
2019-10-26 01:58:44 -0300

    Updates

---
+++ precache-manifest.dc29453eb63f120694f16b92559b3772.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3b554b21a57600860d832fcee86d54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7045f4b5c3f5aa3adc55",
+    "url": "/static/js/2.a589a00c.chunk.js"
+  },
+  {
+    "revision": "f3788f924014b645e035",
+    "url": "/static/js/main.c90e7dd9.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
